f3f40c5ef3adbc85dda44922b8da0f0a586c9977
Gayrat Vlasov <ggvlasov@gmail.com>
2023-05-13 21:46:24 +0300

    feat: добавил простой класс ProxyGet

--- src/amqp-client-proxy-manager.ts
+++ src/amqp-client-proxy-manager.ts
@@ -1,3 +1,4 @@
 export { RMQ_proxyClientQuery } from './rmq-request-responce/clients/clients.js';
 export { proxyRMQnames } from './config/config-rmq.js';
+export { ProxyGet } from './rmq-request-responce/clients/proxy-get.js';
 export type { GetProxyReturn, ParamGetProxy, ParamReturnProxy } from './rmq-request-responce/types/types.js';
---
+++ src/rmq-request-responce/clients/proxy-get.ts
@@ -0,0 +1,56 @@
+import { RMQ_proxyClientQuery } from './clients.js';
+import { proxyRMQnames } from '../../config/config-rmq.js';
+import { GetProxyReturn, ParamGetProxy, ParamReturnProxy } from '../types/types.js';
+
+export class ProxyGet {
+  private instanceRMQ_proxyClientQuery: RMQ_proxyClientQuery;
+  private instance: ProxyGet;
+  private constructor() {}
+  static async getInstance() {
+    const pget = new ProxyGet();
+    pget.instance = pget;
+
+    const cli = await RMQ_proxyClientQuery.createRMQ_clientQuery(
+      proxyRMQnames.exchange,
+      proxyRMQnames.getproxy,
+      proxyRMQnames.getproxy,
+    );
+    pget.instanceRMQ_proxyClientQuery = cli;
+
+    return pget;
+  }
+
+  async getProxy(leasedTime: number) {
+    return this.instanceRMQ_proxyClientQuery.sendRequestAndResieveAnswer<ParamGetProxy, GetProxyReturn>(
+      proxyRMQnames.getproxy,
+      {
+        leasedTime,
+      },
+    );
+  }
+  /*
+пример ответа
+p1.userData.uniqueKey
+
+{ userData:
+   { userData:
+      { server: 'http://104.239.80.193:5771', password: '[***]', username: 'wmdzslaf' },
+     uniqueKey: '6',
+     lastUse: '2023-05-13T18:36:07.796Z',
+     state: 'USING',
+     orderPos: 4,
+     leasedTime: 3000
+     }
+   }
+
+ */
+
+  /*
+    возврат в код происходит почти мгновенно
+   */
+  async returnProxy(p1: GetProxyReturn) {
+    this.instanceRMQ_proxyClientQuery.sendRequestOnly<ParamReturnProxy>(proxyRMQnames.returnProxy, {
+      uniqueKey: p1.userData.uniqueKey,
+    });
+  }
+}
